b93119ad4ea9bff7587ffe74d9538d5b5f045119
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-16 22:14:47 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
+
+            Fibonacci.DisplayFibonacciSeries();
         }
     }
 }
