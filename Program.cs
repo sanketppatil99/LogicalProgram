6d36614cbe4c9138f42f6da535f99bcb4e2c849f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-16 22:23:01 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Logical Programs");
 
-            Fibonacci.DisplayFibonacciSeries();
+            PerfectNumber.FindThePerfectNumber();
         }
     }
 }
